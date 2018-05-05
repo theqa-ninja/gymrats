ff8ebd1c065d482ee54c59451a34750d6bae14c4
Kevin Mark <mrmarkk@amazon.com>
2018-05-05 15:10:19 -0700

    Add register button functionality

--- app/app/src/main/java/com/hackhlth/gymrats/RegisterCoinActivity.java
+++ app/app/src/main/java/com/hackhlth/gymrats/RegisterCoinActivity.java
@@ -2,12 +2,25 @@ package com.hackhlth.gymrats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterCoinActivity extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_coin);
+
+        mButton = findViewById(R.id.button_register_coin);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                Toast.makeText(getApplication(), R.string.register_button_clicked, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
