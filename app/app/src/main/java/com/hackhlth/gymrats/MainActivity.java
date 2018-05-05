f69a353e2100a17fd34f446ff4af22f6b9478d69
Kevin Mark <mrmarkk@amazon.com>
2018-05-05 13:22:09 -0700

    initial view

--- app/app/src/main/java/com/hackhlth/gymrats/MainActivity.java
+++ app/app/src/main/java/com/hackhlth/gymrats/MainActivity.java
@@ -2,12 +2,17 @@ package com.hackhlth.gymrats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mCreatureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCreatureImageView = findViewById(R.id.creature_image_view);
+        mCreatureImageView.setImageDrawable(getResources().getDrawable(R.drawable.whale));
     }
 }
