6199f0e94e1f54bf36833b46f8f10aa1c6f2c123
Hajin Lee <hlee5@zebra.com>
2018-05-05 16:34:45 -0700

    Add recommendation activity. Add log button.

---
+++ app/app/src/main/java/com/hackhlth/gymrats/RecommendationActivity.java
@@ -0,0 +1,13 @@
+package com.hackhlth.gymrats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecommendationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommendation);
+    }
+}
