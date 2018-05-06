df96abfc7de46788d21c26ad30bd26256ebe68b0
jkimball13 <jkimba3@gmail.com>
2018-05-05 20:14:33 -0700

    updates to app build.gradle file to be able to build the app.  Addition of collections page.

--- app/app/src/main/java/com/hackhlth/gymrats/DashboardActivity.java
+++ app/app/src/main/java/com/hackhlth/gymrats/DashboardActivity.java
@@ -1,7 +1,9 @@
 package com.hackhlth.gymrats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        findViewById(R.id.collectionButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DashboardActivity.this, CollectionsActivity.class));
+            }
+        });
     }
 }
