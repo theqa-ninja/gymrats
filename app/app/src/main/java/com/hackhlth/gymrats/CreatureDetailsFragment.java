9ae05356a9a93854c2de83955ab9d92bbf8a3f21
Kevin Mark <mrmarkk@amazon.com>
2018-05-05 17:04:54 -0700

    Creature details

--- app/app/src/main/java/com/hackhlth/gymrats/CreatureDetailsFragment.java
+++ app/app/src/main/java/com/hackhlth/gymrats/CreatureDetailsFragment.java
@@ -3,35 +3,33 @@ package com.hackhlth.gymrats;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class CreatureDetailsFragment extends DialogFragment {
+
+    private ImageView mCreatureImageView;
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity())
-                // Set Dialog Icon
-                // Set Dialog Title
-                .setTitle("Alert DialogFragment")
-                // Set Dialog Message
-                .setMessage("Alert DialogFragment Tutorial")
 
-                // Positive button
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        // Do something else
-                    }
-                })
+        final Dialog dialog = new Dialog(getActivity());
 
-                // Negative Button
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,	int which) {
-                        // Do something else
-                    }
-                }).create();
+        LayoutInflater layoutInflater = (LayoutInflater) getActivity()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View layout = layoutInflater.inflate(R.layout.creaure_details, null);
+        dialog.setContentView(layout);
+
+        ImageView myImage = layout.findViewById(R.id.creature_image_view);
+
+        myImage.setImageDrawable(getResources().getDrawable(R.drawable.whale));
+
+
+        return dialog;
     }
 }
